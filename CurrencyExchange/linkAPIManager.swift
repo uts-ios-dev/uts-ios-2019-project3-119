a4a19de2da32582140973450a7ba4975ec5999cb
mac <mac@192-168-1-173.tpgi.com.au>
2019-05-28 04:13:32 +1000

    change

--- CurrencyExchange/linkAPIManager.swift
+++ CurrencyExchange/linkAPIManager.swift
@@ -5,7 +5,8 @@
 //  Created by UTS on 2019/5/14.
 //  Copyright © 2019 apple. All rights reserved.
 //
-// This is test 222 333
+// This is API links
+
 import UIKit
 import Alamofire
 import SwiftyJSON
